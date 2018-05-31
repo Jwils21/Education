804e47c7d5801fecc10e09f8dbb4d86f7528484d
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-31 16:16:37 -0400

    Update 5/31/2018

--- Education/Program.cs
+++ Education/Program.cs
@@ -7,6 +7,46 @@ using System.Threading.Tasks;
 namespace Education {
 	class Program {
 		static void Main(string[] args) {
+
+			Instructor danTheMan = new Instructor();
+			danTheMan.Name = "Dan";
+			danTheMan.ExperienceInYears = 17;
+			danTheMan.PrimaryLanguage = "Csharp";
+			danTheMan.Email = "dantheman@military.gov";
+			danTheMan.Phone = "123-456-7890";
+
+			Class dotNetBootCamp4 = new Class();
+			dotNetBootCamp4.Capacity = 16;
+			dotNetBootCamp4.Code = "DN4";
+			dotNetBootCamp4.Language = "Csharp";
+			dotNetBootCamp4.Name = "Dot Net Boot Camp 4";
+			dotNetBootCamp4.Instructor = danTheMan;
+
+			Student foster = new Student();
+			foster.Absences = 0;
+			foster.ContactInfo = "Call Foster";
+			foster.FirstJob = DateTime.MinValue;
+			foster.Name = "Foster";
+			foster.Class = dotNetBootCamp4;
+
+			Student jj = new Student();
+			jj.Absences = 0;
+			jj.ContactInfo = "Call JJ";
+			jj.FirstJob = DateTime.MinValue;
+			jj.Name = "Jonathan";
+			jj.Class = dotNetBootCamp4;
+
+			Student mitch = new Student() {
+				FirstJob = DateTime.MinValue,
+				Name = "Mitch",
+				Class = dotNetBootCamp4,
+				Absences = 0,
+				ContactInfo = "Call Mitch"
+			};
+
+			dotNetBootCamp4.Students.Add(foster);
+			dotNetBootCamp4.Students.Add(jj);
+			dotNetBootCamp4.Students.Add(mitch);
+			
 		}
 	}
-}
---
+++ Education/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education {
+	class Student {
+		public String Name { get; set; }
+		public Class Class { get; set; }
+		public String ContactInfo { get; set; }
+		public DateTime FirstJob { get; set; }
+		public int Absences { get; set; }
+	}
+}
